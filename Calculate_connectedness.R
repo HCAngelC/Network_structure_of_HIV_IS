00acbf2a54415b63ee11460dce80b503e475f5d8
Heng-Chang Chen <heng-chang.chen@port.lukasiewicz.gov.pl>
2024-06-07 20:23:00 +0200

    Update Calculate_connectedness.R

--- Calculate_connectedness.R
+++ Calculate_connectedness.R
@@ -4,30 +4,36 @@ rm(list=ls())
 # Author: Heng-Chang Chen
 # Date: 2023.11.28
 ##############################################################
-# Input: Correlation matrix
+# Input: Correlation matrix (undirected)
 # Object: Calculate the connectedness of enriched immunologic signatures
 ##############################################################
 # R functions
 
-vertex_degree_norl <- function(df) {
+vertex_degree_rc <- function(df) {
+  #row
   df_list_row <- data.frame(c(1:309))
   names(df_list_row) <- c("row")
   df_list_row$row <- as.numeric(df_list_row$row)
   
-  df_row.cor <- df %>% dplyr::select(row, cor)
-  df_row.cor.agg <- aggregate(cor ~ row, df_row.cor, mean) %>% dplyr::arrange(row)
+  df_row <- df %>% dplyr::select(row) %>% dplyr::mutate(count_r = 1)
+  df_row.agg <- aggregate(count_r ~ row, df_row, sum) %>% dplyr::arrange(row)
   
-  df_row <- df %>% dplyr::select(row) %>% dplyr::mutate(count = 1)
-  df_row.agg <- aggregate(count ~ row, df_row, sum) %>% dplyr::arrange(row)
+  outer_vertex.r <- dplyr::anti_join(df_list_row, df_row.agg, by = "row") %>% dplyr::mutate(count_r = 0)
+  df_tous.r <- dplyr::bind_rows(df_row.agg, outer_vertex.r) %>% dplyr::arrange(row) %>% dplyr::mutate(ID = c(1:309))
   
-  df_row.sum.cor <- dplyr::bind_cols(df_row.cor.agg, df_row.agg, by = "row") %>% dplyr::mutate(norl = count/cor)
-  df_row.sum.cor_ <- df_row.sum.cor %>% dplyr::select(row...1, norl) %>% dplyr::rename(row = row...1)
+  #column
+  df_list_column <- data.frame(c(1:309))
+  names(df_list_column) <- c("column")
+  df_list_column$column <- as.numeric(df_list_column$column)
   
+  df_column <- df %>% dplyr::select(column) %>% dplyr::mutate(count_c = 1)
+  df_column.agg <- aggregate(count_c ~ column, df_column, sum) %>% dplyr::arrange(column)
   
+
+  outer_vertex.c <- dplyr::anti_join(df_list_column, df_column.agg, by = "column") %>% dplyr::mutate(count_c = 0)
+  df_tous.c <- dplyr::bind_rows(df_column.agg, outer_vertex.c) %>% dplyr::arrange(column) %>% dplyr::mutate(ID = c(1:309))
   
-  outer_vertex <- dplyr::anti_join(df_list_row, df_row.sum.cor_ , by = "row") %>% dplyr::mutate(norl = 0)
-  df_tous <- dplyr::bind_rows(df_row.sum.cor_ , outer_vertex) %>% dplyr::arrange(row)
+  df_tous <- dplyr::bind_cols(df_tous.r, df_tous.c, by = "ID") %>% dplyr::select(row, count_r, column, count_c, ID...3) %>% dplyr::mutate(count_mean = (count_r + count_c)/2) %>% dplyr::rename(ID = ID...3)
   
- 
   return(df_tous)
 }
