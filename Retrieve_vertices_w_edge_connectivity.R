7809bad464aa0cdd15038565e1dd042862e85e88
Heng-Chang Chen <heng-chang.chen@port.lukasiewicz.gov.pl>
2024-06-08 16:41:22 +0200

    Update Retrieve_vertices_w_edge_connectivity.R

--- Retrieve_vertices_w_edge_connectivity.R
+++ Retrieve_vertices_w_edge_connectivity.R
@@ -3,36 +3,36 @@ rm(list=ls())
 ##############################################################
 # Author: Heng-Chang Chen
 ##############################################################
-# Input: A correlation data frame containing every pair of adjacent signatures.
+# Input: (1) df_rank: a data frame containing vertices ID coupled with edge connectivity (data frame output from Calculate_connectedness.R); (2) df_ref: a feature dataframe containing all vertice coupled with features in a network.   
 # Object: A data frame containing the top 10, 20, and 30 ranked vertices with the highest and lowest edge connectivity.
 ##############################################################
 
 # R functions
 
-vertices_connectedness_rank_w_features <- function(df) {
-  df_row <- df %>% dplyr::select(row) %>% dplyr::mutate(count = 1)
-  df_row.agg <- aggregate(count ~ row, df_row, sum) %>% dplyr::arrange(desc(count))
-  df_row.agg.high.top10 <- df_row.agg[1:10,] %>% dplyr::rename(id = row) %>% dplyr::mutate(label = "high")
-  df_row.agg.low.top10 <- tail(df_row.agg, n = 10) %>% dplyr::rename(id = row) %>% dplyr::mutate(label = "low")
-  df_row.agg.all.top10 <- dplyr::bind_rows(df_row.agg.high.top10, df_row.agg.low.top10)
-  df_row.agg.all.top10.features <- dplyr::inner_join(df_complete_2_799, df_row.agg.all.top10, by = "id") %>% dplyr::mutate(top = "10")
+Rank_vertex_selection <- function(df_rank, df_ref) {
+  names(df_rank) <- c("ID", "sum")
+  df_rank <- dplyr::arrange(df_rank, desc(sum))
+  df_rank_high10 <- df_rank[1:10,] %>% dplyr::mutate(rank = "top10", label = "high")
+  df_rank_low10 <- tail(df_rank, n = 10) %>% dplyr::mutate(rank = "top10", label = "low")
   
-   df_row <- df %>% dplyr::select(row) %>% dplyr::mutate(count = 1)
-  df_row.agg <- aggregate(count ~ row, df_row, sum) %>% dplyr::arrange(desc(count))
-  df_row.agg.high.top20 <- df_row.agg[1:20,] %>% dplyr::rename(id = row) %>% dplyr::mutate(label = "high")
-  df_row.agg.low.top20 <- tail(df_row.agg, n = 20) %>% dplyr::rename(id = row) %>% dplyr::mutate(label = "low")
-  df_row.agg.all.top20 <- dplyr::bind_rows(df_row.agg.high.top20, df_row.agg.low.top20)
-  df_row.agg.all.top20.features <- dplyr::inner_join(df_complete_2_799, df_row.agg.all.top20, by = "id") %>% dplyr::mutate(top = "20")
+  df_rank_high20 <- df_rank[1:20,] %>% dplyr::mutate(rank = "top20", label = "high")
+  df_rank_low20 <- tail(df_rank, n = 20) %>% dplyr::mutate(rank = "top20", label = "low")
   
-   df_row <- df %>% dplyr::select(row) %>% dplyr::mutate(count = 1)
-  df_row.agg <- aggregate(count ~ row, df_row, sum) %>% dplyr::arrange(desc(count))
-  df_row.agg.high.top30 <- df_row.agg[1:30,] %>% dplyr::rename(id = row) %>% dplyr::mutate(label = "high")
-  df_row.agg.low.top30 <- tail(df_row.agg, n = 30) %>% dplyr::rename(id = row) %>% dplyr::mutate(label = "low")
-  df_row.agg.all.top30 <- dplyr::bind_rows(df_row.agg.high.top30, df_row.agg.low.top30)
-  df_row.agg.all.top30.features <- dplyr::inner_join(df_complete_2_799, df_row.agg.all.top30, by = "id") %>% dplyr::mutate(top = "30")
+  df_rank_high30 <- df_rank[1:30,] %>% dplyr::mutate(rank = "top30", label = "high")
+  df_rank_low30 <- tail(df_rank, n = 30) %>% dplyr::mutate(rank = "top30", label = "low")
   
-  df_tous <- dplyr::bind_rows(df_row.agg.all.top10.features, df_row.agg.all.top20.features, df_row.agg.all.top30.features)
+  df_ref$ID <- c(1:309)
+  df_rank_high10_m <- dplyr::inner_join(df_ref, df_rank_high10, by = "ID")
+  df_rank_low10_m <- dplyr::inner_join(df_ref, df_rank_low10, by = "ID")
   
-  return(df_tous)
+  df_rank_high20_m <- dplyr::inner_join(df_ref, df_rank_high20, by = "ID")
+  df_rank_low20_m <- dplyr::inner_join(df_ref, df_rank_low20, by = "ID")
+  
+  df_rank_high30_m <- dplyr::inner_join(df_ref, df_rank_high30, by = "ID")
+  df_rank_low30_m <- dplyr::inner_join(df_ref, df_rank_low30, by = "ID")
+  
+  df_wszystko <- dplyr::bind_rows(df_rank_high10_m, df_rank_low10_m, df_rank_high20_m, df_rank_low20_m, df_rank_high30_m, df_rank_low30_m)
+  
+  return(df_wszystko)
 }
 
